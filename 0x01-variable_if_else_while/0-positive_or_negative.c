0a98fb86fade2b8539f841461a834f36694e3391
Abubakar Abdulfatah <abubakrabdul19@outlook.com>
2022-09-10 08:08:47 -0700

    missing stdio.h added

--- 0x01-variable_if_else_while/0-positive_or_negative.c
+++ 0x01-variable_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
@@ -14,11 +15,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("is positive");
+		printf("is positive\n");
 	else if (n == 0)
-		printf("is zero");
+		printf("is zero\n");
 	else
-		printf("is negative");
+		printf("is negative\n");
 	return (0);
 }
 
