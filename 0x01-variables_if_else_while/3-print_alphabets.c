f0d1f06a5c4403447243a02c0e38da054e8d4f0b
Abubakar Abdulfatah <abubakrabdul19@outlook.com>
2022-09-10 21:21:53 -0700

    Task files 1 through 5 successfully debugged

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,16 +5,15 @@
  *
  * Return: 0
  */
-
-int main()
+int main(void)
 {
 	char alp;
 
 	for (alp = 'a'; alp <= 'z'; alp++)
 		putchar(alp);
-	
+
 	for (alp = 'A'; alp <= 'Z'; alp++)
 		putchar(alp);
-	
+	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,13 +5,19 @@
  *
  * Return: 0
  */
-
 int main(void)
 {
 	char alp;
 
 	for (alp = 'a'; alp <= 'z'; alp++)
-		putchar(alp);
+	{
+		if (alp != 'e' && alp != 'q')
+		{
+			putchar(alp);
+		}
+	}
+
 	putchar('\n');
+
 	return (0);
 }
