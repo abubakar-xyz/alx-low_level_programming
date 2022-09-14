3b00b401f36098a6f4445fdf8165671e13c9b9fd
Abubakar Abdulfatah <abubakrabdul19@outlook.com>
2022-09-13 17:13:33 -0700

    task 2 done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints all letters of the alphabets in lowercase10X
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j = 0;
+
+	while (j < 11)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+
+		_putchar('\n');
+		j++;
+	}
+}
