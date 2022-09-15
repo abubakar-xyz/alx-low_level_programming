b16b05c820fa550ed40ef0d7b3ee79d8225bcc9d
Abubakar Abdulfatah <abubakrabdul19@outlook.com>
2022-09-15 15:05:37 -0700

    removed swp file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,16 @@
 
 int print_last_digit(int l)
 {
+	int last_digit;
+
 	while (1)
-		
+	{
+		last_digit = l;
+		if (l == 0)
+		{
+			_putchar(last_digit);
+			break;
+		}
+                l /= 10;
+	}
 }
